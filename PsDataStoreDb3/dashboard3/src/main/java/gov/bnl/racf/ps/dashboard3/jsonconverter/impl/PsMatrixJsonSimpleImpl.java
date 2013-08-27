d42dd7a501bd1cb53fa5d7a74bf8b2b87f6e14b2
TomW <tomw@themistocles.racf.bnl.gov>
2013-08-27 08:29:06 -0400

    finished implementation of PsMatrixJson

--- PsDataStoreDb3/dashboard3/src/main/java/gov/bnl/racf/ps/dashboard3/jsonconverter/impl/PsMatrixJsonSimpleImpl.java
+++ PsDataStoreDb3/dashboard3/src/main/java/gov/bnl/racf/ps/dashboard3/jsonconverter/impl/PsMatrixJsonSimpleImpl.java
@@ -4,9 +4,15 @@
  */
 package gov.bnl.racf.ps.dashboard3.jsonconverter.impl;
 
+import gov.bnl.racf.ps.dashboard3.domainobjects.PsHost;
 import gov.bnl.racf.ps.dashboard3.domainobjects.PsMatrix;
+import gov.bnl.racf.ps.dashboard3.domainobjects.PsService;
+import gov.bnl.racf.ps.dashboard3.jsonconverter.PsHostJson;
 import gov.bnl.racf.ps.dashboard3.jsonconverter.PsMatrixJson;
+import gov.bnl.racf.ps.dashboard3.jsonconverter.PsServiceJson;
 import gov.bnl.racf.ps.dashboard3.parameters.PsParameters;
+import gov.bnl.racf.utils.IsoDateConverter;
+import java.util.Date;
 import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -16,6 +22,20 @@ import org.json.simple.JSONObject;
  * @author tomw
  */
 public class PsMatrixJsonSimpleImpl implements PsMatrixJson{
+    
+    //=== dependency injection ===//
+    private PsHostJson psHostJson;
+
+    public void setPsHostJson(PsHostJson psHostJson) {
+        this.psHostJson = psHostJson;
+    }
+    
+    private PsServiceJson psServiceJson;
+
+    public void setPsServiceJson(PsServiceJson psServiceJson) {
+        this.psServiceJson = psServiceJson;
+    }
+    
 
     /**
      * convert matrix to JSONObject
@@ -35,7 +55,84 @@ public class PsMatrixJsonSimpleImpl implements PsMatrixJson{
      */
     @Override
     public JSONObject toJson(PsMatrix matrix, String detailLevel) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        
+        JSONObject json = new JSONObject();
+        if (matrix != null) {
+            json.put(PsMatrix.ID, ""+matrix.getId());
+            json.put(PsMatrix.NAME, matrix.getName());
+
+            if (!PsParameters.DETAIL_LEVEL_LOW.equals(detailLevel)) {
+
+                json.put(PsMatrix.DETAIL_LEVEL, matrix.getDetailLevel());
+                json.put(PsMatrix.SERVICE_TYPE_ID, matrix.getMatrixType().getServiceTypeId());
+
+                List<String> statusLabels = matrix.getStatusLabels();
+                JSONArray jsonArray = new JSONArray();
+                if (statusLabels != null) {
+                    if (!statusLabels.isEmpty()) {
+                        for (int i = 0; i < statusLabels.size(); i = i + 1) {
+                            jsonArray.add(statusLabels.get(i));
+                        }
+                    }
+                }
+                json.put(PsMatrix.STATUS_LABELS, jsonArray);
+
+                Date lastUpdateTime = matrix.getLastUpdateTime();
+                json.put(PsMatrix.LAST_UPDATE_TIME,
+                        IsoDateConverter.dateToString(lastUpdateTime));
+
+                JSONArray rows = new JSONArray();
+                for (int i = 0; i < matrix.getNumberOfRows(); i = i + 1) {
+                    PsHost currentHost = matrix.getHostInRow(i);
+                    JSONObject hostAsJson = this.psHostJson.toJson(currentHost,PsParameters.DETAIL_LEVEL_LOW);
+                    //rows.add(currentHost.getHostname());
+                    rows.add(hostAsJson);
+                }
+                json.put(PsMatrix.ROWS, rows);
+
+                JSONArray columns = new JSONArray();
+                for (int i = 0; i < matrix.getNumberOfColumns(); i = i + 1) {
+                    PsHost currentHost = matrix.getHostInColumn(i);
+                    JSONObject hostAsJson = this.psHostJson.toJson(currentHost,PsParameters.DETAIL_LEVEL_LOW);
+                    //columns.add(currentHost.getHostname());
+                    columns.add(hostAsJson);
+                }
+                json.put(PsMatrix.COLUMNS, columns);
+
+                JSONArray serviceNames = new JSONArray();
+                for (int i = 0; i < matrix.getNumberOfServiceNames(); i = i + 1) {
+                    serviceNames.add(matrix.getServiceNames().get(i));
+                }
+                json.put(PsMatrix.SERVICE_NAMES, serviceNames);
+
+                JSONArray matrixArray = new JSONArray();
+                for (int i = 0; i < matrix.getNumberOfColumns(); i = i + 1) {
+                    JSONArray rowsArray = new JSONArray();
+                    for (int j = 0; j < matrix.getNumberOfRows(); j = j + 1) {
+                        JSONArray serviceArray = new JSONArray();
+                        for (int k = 0; k < matrix.getNumberOfServiceNames(); k = k + 1) {
+                            PsService currentService = matrix.getService(i, j, k);
+
+                            String serviceDetailLevel = PsParameters.DETAIL_LEVEL_HIGH;
+                            if (PsParameters.DETAIL_LEVEL_MEDIUM.equals(detailLevel)) {
+                                serviceDetailLevel = PsParameters.DETAIL_LEVEL_LOW;
+                            }
+                            if (PsParameters.DETAIL_LEVEL_HIGH.equals(detailLevel)) {
+                                serviceDetailLevel = PsParameters.DETAIL_LEVEL_HIGH;
+                            }
+                            JSONObject currentServiceJson =
+                                    this.psServiceJson.toJson(currentService, serviceDetailLevel);
+                            serviceArray.add(currentServiceJson);
+                        }
+                        rowsArray.add(serviceArray);
+                    }
+                    matrixArray.add(rowsArray);
+                }
+                json.put(PsMatrix.MATRIX, matrixArray);
+            }
+        }
+
+        return json;
     }
 
     /**
