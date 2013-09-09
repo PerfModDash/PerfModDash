aba6d3dc0a311ae371e5946fdc189f0f35e05fb3
TomW <tomw@themistocles.racf.bnl.gov>
2013-09-09 13:19:57 -0400

    some bugs in bean definitions fixed; testing started; initServiceTypes works; GET hosts works; tests to be continued...

--- PsDataStoreDb3/dashboard3/src/main/java/gov/bnl/racf/ps/dashboard3/control/UtilityController.java
+++ PsDataStoreDb3/dashboard3/src/main/java/gov/bnl/racf/ps/dashboard3/control/UtilityController.java
@@ -4,6 +4,8 @@
  */
 package gov.bnl.racf.ps.dashboard3.control;
 
+import gov.bnl.racf.ps.dashboard3.domainobjects.PsHost;
+import gov.bnl.racf.ps.dashboard3.operators.PsHostOperator;
 import gov.bnl.racf.ps.dashboard3.operators.PsServiceTypeOperator;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,14 +15,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * Controller for performing utility operations, like initialisation of service types or deleting entire datastore.
- * It is meant to be used during development process to help with debugging. It will be deleted when the datastore is deployed for production. 
- * The corresponding url's do not form part of the official API and should not be included in any documentation.
+ * Controller for performing utility operations, like initialisation of service
+ * types or deleting entire datastore. It is meant to be used during development
+ * process to help with debugging. It will be deleted when the datastore is
+ * deployed for production. The corresponding url's do not form part of the
+ * official API and should not be included in any documentation.
+ *
  * @author tomw
  */
 @Controller
 @RequestMapping(value = "/util")
 public class UtilityController {
+
     @Autowired
     private PsServiceTypeOperator psServiceTypeOperator;
 
@@ -28,30 +34,41 @@ public class UtilityController {
         this.psServiceTypeOperator = psServiceTypeOperator;
     }
     
+    @Autowired
+    private PsHostOperator psHostOperator;
+
+    public void setPsHostOperator(PsHostOperator psHostOperator) {
+        this.psHostOperator = psHostOperator;
+    }
+
     /**
      * method to initialize the service types in database
-     * @return 
+     *
+     * @return
      */
     @RequestMapping(value = "/initServiceTypes", method = RequestMethod.GET)
     @ResponseBody
     public String initServiceTypes() {
         String message = "we are in initServiceTypes()<BR>";
-        List<String>listOfServiceTypeNames = this.psServiceTypeOperator.initServiceTypes();
-        for(String serviceTypeName : listOfServiceTypeNames){
-            message=message+" "+serviceTypeName;
+        List<String> listOfServiceTypeNames = this.psServiceTypeOperator.initServiceTypes();
+        for (String serviceTypeName : listOfServiceTypeNames) {
+            message = message + " " + serviceTypeName;
         }
         return message;
     }
-    
+
     /**
      * debug method to dump content of database in human readable format
-     * @return 
+     *
+     * @return
      */
     @RequestMapping(value = "/dump", method = RequestMethod.GET)
     @ResponseBody
     public String dump() {
         //TODO write this method!!!
-        String message = "we are in dump()";
+        String message = "";
+        List<PsHost> listOfHosts = this.psHostOperator.getAll();
+        message = message + "<H1>HOSTS</H1>" + this.psHostOperator.toJson(listOfHosts).toString();
         return message;
     }
 }
